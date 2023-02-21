9af04c60e093953894fac7faaf743011fc6c4a02
maevahrs <m.hiersoso972@gmail.com>
2023-02-21 12:38:16 +0100

    modification page d'acceuil

--- app/src/main/java/com/example/to_do_list/MainActivity.kt
+++ app/src/main/java/com/example/to_do_list/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.to_do_list
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main);
     }
 }

7cfef003b1b0b6fc23c066819dad4cbde1f5cd55
Léo Quenette <leo.quenette@etu.unilim.fr>
2023-02-21 10:54:27 +0100

    Création du boutton d'ajout de taches

--- app/src/main/java/com/example/to_do_list/MainActivity.kt
+++ app/src/main/java/com/example/to_do_list/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.to_do_list
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var addTaskBtn = findViewById<ImageButton>(R.id.imageButton)
+        addTaskBtn.setOnClickListener(
+            setContentView(R.layout.activity_add_task)
+        )
     }
 }

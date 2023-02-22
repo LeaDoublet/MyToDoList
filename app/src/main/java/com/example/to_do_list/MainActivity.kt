6dd445180290755992fae5644df2714e52a7c880
maevahrs <m.hiersoso972@gmail.com>
2023-02-22 14:00:56 +0100

    modification page d'acceuil

--- app/src/main/java/com/example/to_do_list/MainActivity.kt
+++ app/src/main/java/com/example/to_do_list/MainActivity.kt
@@ -1,20 +1,23 @@
 package com.example.to_do_list
 
+import android.annotation.SuppressLint
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var addTaskBtn = findViewById<ImageButton>(R.id.imageButton)
-        var changeClothesBtn = findViewById<ImageButton>(R.id.imageButton3)
-        addTaskBtn.setOnClickListener{
-            setContentView(R.layout.activity_add_task)
+        val addTask = findViewById<ImageButton>(R.id.addTask)
+        val goPageClothe = findViewById<ImageButton>(R.id.goPageClothe)
+
+        goPageClothe.setOnClickListener{
+            setContentView(R.layout.activity_change_clothe)
         }
-        changeClothesBtn.setOnClickListener {
-        setContentView(R.layout.activity_change_clothe)
-        }
+
     }
 }

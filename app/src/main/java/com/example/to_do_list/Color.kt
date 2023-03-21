4467c3964dd313601a45467f9863ccecd15add33
maevahrs <m.hiersoso972@gmail.com>
2023-03-21 19:24:45 +0100

    ajout page vue de tous les tache

--- app/src/main/java/com/example/to_do_list/Color.kt
+++
@@ -1,9 +0,0 @@
-package com.example.to_do_list
-
-enum class Color (val rgb: Int){
-    TASK1(0xFCD8CA),
-    TASK2(0xE5C7BB),
-    PURPLE(0xFF99FF),
-    TASK3(0xD6B5A7),
-    WHITE(0x000000)
-}
--- app/src/main/java/com/example/to_do_list/MainActivity.kt
+++ app/src/main/java/com/example/to_do_list/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.example.to_do_list
 
 import android.annotation.SuppressLint
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.*
+import android.content.Intent
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val addTask = findViewById<ImageButton>(R.id.addTask)
         val goPageClothe = findViewById<ImageButton>(R.id.goPageClothe)
+        val goViewModel = findViewById<ImageButton>(R.id.imagecat)
 
         goPageClothe.setOnClickListener{
             val intent = Intent(this@MainActivity, ChangeClothesActivity::class.java)
@@ -25,53 +26,11 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-    }
-
-    fun saveRecord(view: View) {
-        val id = findViewById<EditText>(R.id.u_id).text.toString()
-        val state = findViewById<EditText>(R.id.state).text.toString()
-        val titre = findViewById<EditText>(R.id.titreNewtask).text.toString()
-        val description = findViewById<EditText>(R.id.descriptionNewTask).text.toString()
-        val couleur = findViewById<EditText>(R.id.color).text.toString()
-        val date= findViewById<EditText>(R.id.date).text.toString()
-        val databaseHandler= DatabaseHandler(this)
-
-        val idMax = databaseHandler.lastID()
-
-        if(id.trim()!="" && titre.trim()!="" && state.trim()!=""){
-            val Taski = Task(idMax,"TODO","titre","description", 0xFCD8CA,"10/03/2023")
-            val status = databaseHandler.addTask(Taski)
-            if(status > -1){
-                Toast.makeText(applicationContext,"record save", Toast.LENGTH_LONG).show()
-                findViewById<EditText>(R.id.u_id).text.clear()
-                findViewById<EditText>(R.id.state).text.clear()
-                findViewById<EditText>(R.id.titre).text.clear()
-                findViewById<EditText>(R.id.description).text.clear()
-                findViewById<EditText>(R.id.color).text.clear()
-                findViewById<EditText>(R.id.date).text.clear()
-            }
-        }else{
-            Toast.makeText(applicationContext,"id or name or email cannot be blank", Toast.LENGTH_LONG).show()
+        goViewModel.setOnClickListener{
+            val intent = Intent(this@MainActivity, ListTaskActivity::class.java)
+            startActivity(intent)
         }
+
     }
 
-    fun viewRecord(view: View){
-        //creating the instance of DatabaseHandler class
-        val databaseHandler: DatabaseHandler= DatabaseHandler(this)
-        //calling the viewEmployee method of DatabaseHandler class to read the records
-        val emp: List<Task> = databaseHandler.viewEmployee()
-        val empArrayId = Array<String>(emp.size){"0"}
-        val empArrayName = Array<String>(emp.size){"null"}
-        val empArrayEmail = Array<String>(emp.size){"null"}
-        var index = 0
-        for(e in emp){
-            empArrayId[index] = e.id.toString()
-            empArrayName[index] = e.titre
-            empArrayEmail[index] = e.description
-            index++
-        }
-        //creating custom ArrayAdapter
-        val myListAdapter = MyListAdapter(this,empArrayId,empArrayName,empArrayEmail)
-        findViewById<ListView>(R.id.listView).adapter = myListAdapter
-    }
 }

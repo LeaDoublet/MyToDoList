f204271dbf739f5350ce36cb357f68f169a3d52e
maevahrs <m.hiersoso972@gmail.com>
2023-03-22 09:44:19 +0100

    correction problème au niveaux du l'affichage des taches

--- app/src/main/java/com/example/to_do_list/ListTaskActivity.kt
+++ app/src/main/java/com/example/to_do_list/ListTaskActivity.kt
@@ -3,6 +3,7 @@ package com.example.to_do_list
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
@@ -24,53 +25,59 @@ class ListTaskActivity: AppCompatActivity() {
         val empArrayId_TODO = Array<String>(emp.size){"0"}
         val empArrayTitre_TODO = Array<String>(emp.size){"null"}
         val empArrayDescription_TODO = Array<String>(emp.size){"null"}
+
         val empArrayState = Array<String>(emp.size){"null"}
         val empArrayId_LATE = Array<String>(emp.size){"0"}
         val empArrayTitre_LATE = Array<String>(emp.size){"null"}
         val empArrayDescription_LATE = Array<String>(emp.size){"null"}
+
         val empArrayId_FINISHED = Array<String>(emp.size){"0"}
         val empArrayTitre_FINISHED = Array<String>(emp.size){"null"}
         val empArrayDescription_FINISHED = Array<String>(emp.size){"null"}
 
-        for((index, e) in emp.withIndex()){
-            if (e.state == "TODO"){
-                empArrayId_TODO[index] = e.id.toString()
-                empArrayTitre_TODO[index] = e.titre
-                empArrayDescription_TODO[index] = e.description
-                empArrayState[index] = e.state
+        if (!emp.isEmpty()){
+            for((index, e) in emp.withIndex()){
+                if (e.state == "TODO"){
+                    empArrayId_TODO[index] = e.id.toString()
+                    empArrayTitre_TODO[index] = e.titre
+                    empArrayDescription_TODO[index] = e.description
+                    empArrayState[index] = e.state
+                }
+                if (e.state == "LATE"){
+                    empArrayId_LATE[index] = e.id.toString()
+                    empArrayTitre_LATE[index] = e.titre
+                    empArrayDescription_LATE[index] = e.description
+                    empArrayState[index] = e.state
+                }
+                if (e.state == "FINISHED"){
+                    empArrayId_FINISHED[index] = e.id.toString()
+                    empArrayTitre_FINISHED[index] = e.titre
+                    empArrayDescription_FINISHED[index] = e.description
+                    empArrayState[index] = e.state
+                }
             }
-            if (e.state == "LATE"){
-                empArrayId_LATE[index] = e.id.toString()
-                empArrayTitre_LATE[index] = e.titre
-                empArrayDescription_LATE[index] = e.description
-            }
-            if (e.state == "FINISHED"){
-                empArrayId_FINISHED[index] = e.id.toString()
-                empArrayTitre_FINISHED[index] = e.titre
-                empArrayDescription_FINISHED[index] = e.description
-            }
-
         }
+
         //creating custom ArrayAdapter
         val myListAdapter_TODO = MyListAdapter(this,empArrayId_TODO,empArrayTitre_TODO,empArrayDescription_TODO,empArrayState)
         val myListAdapter_LATE = MyListAdapter(this,empArrayId_LATE,empArrayTitre_LATE,empArrayDescription_LATE,empArrayState)
         val myListAdapter_FINISHED = MyListAdapter(this,empArrayId_FINISHED,empArrayTitre_FINISHED,empArrayDescription_FINISHED,empArrayState)
-        if (empArrayId_LATE[0].contentEquals("0")){
-
-        }else{
-            findViewById<ListView>(R.id.listViewLATE).adapter = myListAdapter_LATE
+        if (!emp.isEmpty()){
+            if (empArrayId_LATE[0].contentEquals("0")){
+                Log.d(empArrayId_LATE.toString(),"this contenValue")
+            }else{
+                findViewById<ListView>(R.id.listViewLATE).adapter = myListAdapter_LATE
+            }
+            if (empArrayId_TODO[0].contentEquals("0")){
+                Log.d(empArrayId_TODO.toString(),"this contenValue")
+            }else{
+                findViewById<ListView>(R.id.listViewTODO).adapter = myListAdapter_TODO
+            }
+            if (empArrayId_FINISHED[0].contentEquals("0")){
+                Log.d(empArrayId_FINISHED.toString(),"this contenValue")
+            }else{
+                findViewById<ListView>(R.id.listViewFINISH).adapter = myListAdapter_FINISHED
+            }
         }
-        if (empArrayId_TODO[0].contentEquals("0")){
-
-        }else{
-            findViewById<ListView>(R.id.listViewTODO).adapter = myListAdapter_TODO
-        }
-        if (empArrayId_FINISHED[0].contentEquals("0")){
-
-        }else{
-            findViewById<ListView>(R.id.listViewFINISH).adapter = myListAdapter_FINISHED
-        }
-
-
     }
 }
